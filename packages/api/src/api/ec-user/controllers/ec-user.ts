d7965569b182420c04c3b603b2711b1c2af69477
estradax <azharumuhammad@gmail.com>
2024-02-09 17:08:56 +0700

    feat: first commit

---
+++ packages/api/src/api/ec-user/controllers/ec-user.ts
@@ -0,0 +1,7 @@
+/**
+ * ec-user controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::ec-user.ec-user');
---
+++ packages/api/src/api/ec-user/routes/ec-user.ts
@@ -0,0 +1,7 @@
+/**
+ * ec-user router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::ec-user.ec-user');
