8d4aa67dd87f1eae7a7d70fcf8aeff0543b91672
estradax <azharumuhammad@gmail.com>
2024-02-24 19:08:26 +0700

    feat(app): add fab

--- packages/app/lib/screens/schools_screen.dart
+++ packages/app/lib/screens/schools_screen.dart
@@ -1,4 +1,3 @@
-import 'package:educonnect_app/widgets/ec_text_form_field.dart';
 import 'package:flutter/material.dart';
 
 class SchoolsScreen extends StatelessWidget {
@@ -6,58 +5,139 @@ class SchoolsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              style: TextStyle(
-                fontSize: 12,
-              ),
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.all(8),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(22),
-                  borderSide: BorderSide(
-                    color: Colors.black.withOpacity(0.3),
-                    width: 0.5,
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(22),
-                  borderSide: BorderSide(
-                    color: Colors.black.withOpacity(0.3),
-                    width: 0.5,
-                  ),
-                ),
-                labelText: 'Search School',
-                labelStyle: TextStyle(
-                  fontSize: 12,
-                  color: Colors.black.withOpacity(0.5),
-                ),
-                floatingLabelBehavior: FloatingLabelBehavior.never,
-                prefixIcon: Icon(Icons.search),
-              ),
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            EcSchoolCard(
-              name: 'SMPN 44 Bandung',
-              address: 'Jl.Cihaur Geulis No 14, Tongkeng, Bandung, Jawa Barat',
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            EcSchoolCard(
-              name: 'SMA Al-Irsyad Satya',
-              address: 'Jl.Lengkong Kecil No 14, Lengkong, Jakarta, Jawa Barat',
-            ),
-          ],
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('xxx');
+        },
+        child: Image.asset(
+          'lib/assets/images/fab.png',
+          width: 32,
+          height: 32,
+        ),
+        backgroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            width: 1,
+            color: Colors.black.withOpacity(0.3),
+          ),
+          borderRadius: BorderRadius.circular(8),
         ),
       ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                style: const TextStyle(
+                  fontSize: 12,
+                ),
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(8),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(22),
+                    borderSide: BorderSide(
+                      color: Colors.black.withOpacity(0.3),
+                      width: 0.5,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(22),
+                    borderSide: BorderSide(
+                      color: Colors.black.withOpacity(0.3),
+                      width: 0.5,
+                    ),
+                  ),
+                  labelText: 'Search School',
+                  labelStyle: TextStyle(
+                    fontSize: 12,
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  prefixIcon: const Icon(Icons.search),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Row(
+                children: [
+                  EcSchoolIndicator(
+                    name: 'Need Teachers',
+                    color: Colors.red,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  EcSchoolIndicator(
+                    name: 'Need Facilities',
+                    color: Colors.yellow,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  EcSchoolIndicator(
+                    name: 'Fullfilled',
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              const EcSchoolCard(
+                name: 'SMPN 44 Bandung',
+                address:
+                    'Jl.Cihaur Geulis No 14, Tongkeng, Bandung, Jawa Barat',
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const EcSchoolCard(
+                name: 'SMA Al-Irsyad Satya',
+                address:
+                    'Jl.Lengkong Kecil No 14, Lengkong, Jakarta, Jawa Barat',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class EcSchoolIndicator extends StatelessWidget {
+  final String name;
+  final Color color;
+
+  const EcSchoolIndicator({
+    super.key,
+    required this.name,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 10,
+          height: 10,
+          decoration: BoxDecoration(
+            color: color,
+            shape: BoxShape.circle,
+          ),
+        ),
+        const SizedBox(
+          width: 4,
+        ),
+        Text(
+          name,
+          style: const TextStyle(fontSize: 8),
+        ),
+      ],
     );
   }
 }
@@ -79,32 +159,34 @@ class EcSchoolCard extends StatelessWidget {
         border: Border.all(),
         borderRadius: BorderRadius.circular(8),
       ),
-      padding: EdgeInsets.all(12),
+      padding: const EdgeInsets.all(12),
       child: Row(
         children: [
-          Image(
+          const Image(
             image: AssetImage('lib/assets/images/sch-icon.png'),
             width: 38,
             height: 38,
           ),
-          SizedBox(
+          const SizedBox(
             width: 10,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                name,
-                style: TextStyle(fontSize: 10),
-              ),
-              Text(
-                address,
-                style: TextStyle(
-                  fontSize: 8,
-                  color: Colors.black.withOpacity(0.6),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: const TextStyle(fontSize: 10),
                 ),
-              ),
-            ],
+                Text(
+                  address,
+                  style: TextStyle(
+                    fontSize: 8,
+                    color: Colors.black.withOpacity(0.6),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
